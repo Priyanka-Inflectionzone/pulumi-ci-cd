0be494e6023dc1bdadc9133e11d0b878acea9fbf
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-05-04 12:13:01 +0530

    modified userdata

--- index.ts
+++ index.ts
@@ -126,14 +126,7 @@ const devSG = new aws.ec2.SecurityGroup("dev-sg", {
 const userData= 
 `#!/bin/bash
 apt-get update
-apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install -y docker-ce
+apt-get install -y docker.io
 usermod -aG docker ubuntu
 chmod 666 /var/run/docker.sock
 apt-get install -y awscli 
